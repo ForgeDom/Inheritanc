281b66fa6035de7a641697a28b4d3135839055ae
ForgeDom <maxstill2006@gmail.com>
2025-02-24 20:39:39 +0200

    +

--- Classes/Class.cs
+++ Classes/Class.cs
@@ -1,12 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Inheritanc.Classes
 {
-    internal class Class
+    public enum Currency
     {
+        USD,
+        EUR,
+        GBP,
+        JPY
+    }
+    public enum Cent
+    {
+        Cents,
+        Pence,
+        EuroCent,
+        Yen
+    }
+    public class Money
+    {
+        public int WholePart { get; set; }
+        public int FractionalPart { get; set; }
+        public Currency CurrencyType { get; set; }
+        public Cent CentType { get; set; }
+        public Money(int WwolePart, int fractionalPart, Currency currencyType, Cent centType)
+        {
+            WholePart = WwolePart;
+            FractionalPart = fractionalPart;
+            CurrencyType = currencyType;
+            CentType = centType;
+        }
+        public void SetMoney(int wholePart, int fractionalPart, Currency currencyType, Cent centType)
+        {
+            WholePart = wholePart;
+            FractionalPart = fractionalPart;
+            CurrencyType = currencyType;
+            CentType = centType;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{WholePart} {CurrencyType} and {FractionalPart} {CentType}");
+        }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public Money Price { get; set; }
+        public Product(string name, Money price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public void ReducePrice(int wholePart, int fractionalPart)
+        {
+            int totalFractionalPart = Price.FractionalPart - fractionalPart;
+            int totalWholePart = Price.WholePart - wholePart;
+
+            if (totalFractionalPart < 0)
+            {
+                totalWholePart -= 1;
+                totalFractionalPart += 100;
+            }
+
+            Price.SetMoney(totalWholePart, totalFractionalPart, Price.CurrencyType, Price.CentType);
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Product: {Name}");
+            Price.Display();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Inheritanc
     {
         static void Main(string[] args)
         {
-            
+            Money money = new Money(10, 50, Currency.USD, Cent.Cents);
+            money.Display();
+
+            Product product = new Product("Laptop", money);
+            product.ReducePrice(5, 20);
+            product.Display();
+
         }
     }
 }
