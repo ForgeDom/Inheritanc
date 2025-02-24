6876aae6ffd62778818a3692e1c44156b7553c60
ForgeDom <maxstill2006@gmail.com>
2025-02-24 20:20:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Inheritanc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
