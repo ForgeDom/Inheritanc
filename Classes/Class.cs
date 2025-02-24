924868420a7488727747f9bda575151f0a5ebf03
ForgeDom <maxstill2006@gmail.com>
2025-02-24 20:23:47 +0200

    +

--- Classes/Class.cs
+++ Classes/Class.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Inheritanc.Classes
 {
-    internal class Class
+    public class Human
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Surname: {Surname}, Age: {Age}, Gender: {Gender}");
+        }
+    }
+
+    public class Builder : Human
+    {
+        public string Profession { get; } = "Builder";
+
+        public void Build()
+        {
+            Console.WriteLine($"{Name} is building.");
+        }
+    }
+
+    public class Sailor : Human
+    {
+        public string Profession { get; } = "Sailor";
+
+        public void Sail()
+        {
+            Console.WriteLine($"{Name} is sailing.");
+        }
+    }
+
+    public class Pilot : Human
+    {
+        public string Profession { get; } = "Pilot";
+
+        public void Fly()
+        {
+            Console.WriteLine($"{Name} is flying.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Inheritanc.Classes;
+using System.Security.Cryptography;
 
 namespace Inheritanc
 {
@@ -6,7 +7,44 @@ namespace Inheritanc
     {
         static void Main(string[] args)
         {
-            
+            Human human = new Human
+            {
+                Name = "John",
+                Surname = "Doe",
+                Age = 30,
+                Gender = "Male"
+            };
+            human.DisplayInfo();
+
+            Builder builder = new Builder
+            {
+                Name = "Bob",
+                Surname = "Pagani",
+                Age = 45,
+                Gender = "Male"
+            };
+            builder.DisplayInfo();
+            builder.Build();
+
+            Sailor sailor = new Sailor
+            {
+                Name = "Jack",
+                Surname = "Sparrow",
+                Age = 50,
+                Gender = "Male"
+            };
+            sailor.DisplayInfo();
+            sailor.Sail();
+
+            Pilot pilot = new Pilot
+            {
+                Name = "Siri",
+                Surname = "Travolta",
+                Age = 60,
+                Gender = "Female"
+            };
+            pilot.DisplayInfo();
+            pilot.Fly();
         }
     }
 }
