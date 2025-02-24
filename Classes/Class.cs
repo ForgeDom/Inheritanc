549c7c18f61c947ae97440e482d7a4a07e7fcbb8
ForgeDom <maxstill2006@gmail.com>
2025-02-24 20:24:54 +0200

    +

--- Classes/Class.cs
+++ Classes/Class.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Inheritanc.Classes
 {
-    internal class Class
+    public class Passport
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string BirthDate { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public void Display()
+        {
+            Console.WriteLine("Passport Information");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"BirthDate: {BirthDate}");
+            Console.WriteLine($"Country: {Country}");
+            Console.WriteLine($"City: {City}");
+        }
+    }
+
+    public class ForeignPassport : Passport
+    {
+        public string Visa { get; set; }
+        public string ExpiryDate { get; set; }
+        public string PassportNumber { get; set; }
+        public new void Display()
+        {
+            base.Display();
+            Console.WriteLine("Foreign Passport Information");
+            Console.WriteLine($"Visa: {Visa}");
+            Console.WriteLine($"ExpiryDate: {ExpiryDate}");
+            Console.WriteLine($"PassportNumber: {PassportNumber}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace Inheritanc
     {
         static void Main(string[] args)
         {
-            
+            Passport passport = new Passport
+            {
+                Name = "John",
+                Surname = "Doe",
+                BirthDate = "01/01/1990",
+                Country = "USA",
+                City = "New York"
+            };
+            passport.Display();
+
+            Console.WriteLine();
+
+            ForeignPassport foreignPassport = new ForeignPassport
+            {
+                Name = "Jane",
+                Surname = "Smith",
+                BirthDate = "02/02/1985",
+                Country = "USA",
+                City = "Los Angeles",
+                Visa = "Tourist",
+                ExpiryDate = "12/31/2025",
+                PassportNumber = "A12345678"
+            };
+            foreignPassport.Display();
         }
     }
 }
