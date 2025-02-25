57d247b8144c8606d624be9ba391b7e5a955da13
ForgeDom <maxstill2006@gmail.com>
2025-02-25 20:26:06 +0200

    +

--- Classes/Class.cs
+++ Classes/Class.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace Inheritanc.Classes
 {
-    internal class Class
+    public class  MusicalInstrument
     {
+        private string Name { get; set; }
+        private string Type { get; set; }
+        private string Sound { get; set; }
+        private string Description { get; set; }
+        private string History { get; set; }
+        public MusicalInstrument(string name, string type, string sound, string description, string history)
+        {
+            Name = name;
+            Type = type;
+            Sound = sound;
+            Description = description;
+            History = history;
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine(Sound);
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}, Type: {Type}");
+        }
+
+        public void Desc()
+        {
+            Console.WriteLine(Description);
+        }
+
+        public void HistoryInfo()
+        {
+            Console.WriteLine(History);
+        }
+    }
+
+    public class Skripka : MusicalInstrument
+    {
+        public Skripka(string name, string type, string sound, string description, string history)
+            : base(name, type, sound, description, history) { }
+    }
+
+    public class Trambon : MusicalInstrument
+    {
+        public Trambon(string name, string type, string sound, string description, string history)
+            : base(name, type, sound, description, history) { }
+    }
+
+    public class Okulele : MusicalInstrument
+    {
+        public Okulele(string name, string type, string sound, string description, string history)
+            : base(name, type, sound, description, history) { }
+    }
+
+    public class Violonchel : MusicalInstrument
+    {
+        public Violonchel(string name, string type, string sound, string description, string history)
+            : base(name, type, sound, description, history) { }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Inheritanc.Classes;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Inheritanc
 {
@@ -6,7 +7,24 @@ namespace Inheritanc
     {
         static void Main(string[] args)
         {
-            
+            Skripka skripka = new Skripka("Skripka", "Strunniy", "Skripka", "Skripka is a string instrument", "Skripka is a very old instrument");
+            Trambon trambon = new Trambon("Trambon", "Duhovoy", "Trambon", "Trambon is a wind instrument", "Trambon is a very old instrument");
+            Okulele okulele = new Okulele("Okulele", "Strunniy", "Okulele", "Okulele is a string instrument", "Okulele is a very old instrument");
+            Violonchel violonchel = new Violonchel("Violonchel", "Strunniy", "Violonchel", "Violonchel is a string instrument", "Violonchel is a very old instrument");
+
+            Display(skripka);
+            Display(trambon);
+            Display(okulele);
+            Display(violonchel);
+
+            static void Display(MusicalInstrument musicalInstrument)
+            {
+                musicalInstrument.Show();
+                musicalInstrument.Desc();
+                musicalInstrument.HistoryInfo();
+                musicalInstrument.MakeSound();
+            }
+
         }
     }
 }
