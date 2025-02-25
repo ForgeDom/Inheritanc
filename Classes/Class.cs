cb7bf76f221604b624f52249fd1bf06f2fcacde1
ForgeDom <maxstill2006@gmail.com>
2025-02-25 20:29:28 +0200

    +

--- Classes/Class.cs
+++ Classes/Class.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Inheritanc.Classes
 {
-    internal class Class
+    public abstract class Worker
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+        public Worker(string name, int age, string position, double salary)
+        {
+            Name = name;
+            Age = age;
+            Position = position;
+            Salary = salary;
+        }
+        public abstract void Print();
+    }
+
+    public class President : Worker
+    {
+        public President(string name, int age, string position, double salary) : base(name, age, position, salary)
+        {
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Position: {Position}, Salary: {Salary}");
+        }
+    }
+    public class Manager : Worker
+    {
+        public Manager(string name, int age, string position, double salary) : base(name, age, position, salary)
+        {
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Position: {Position}, Salary: {Salary}");
+        }
+    }
+
+    public class Engineer : Worker
+    {
+        public Engineer(string name, int age, string position, double salary) : base(name, age, position, salary)
+        {
+        }
+        public override void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}, Position: {Position}, Salary: {Salary}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Inheritanc
     {
         static void Main(string[] args)
         {
-            
+            President president = new President("John", 45, "President", 100000);
+            Manager manager = new Manager("Jane", 35, "Manager", 50000);
+            Engineer engineer = new Engineer("Jack", 25, "Engineer", 30000);
+
+            president.Print();
+            manager.Print();
+            engineer.Print();
         }
     }
 }
