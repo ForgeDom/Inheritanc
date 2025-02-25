8d13ecfd5905df5c54ca7e70eaea8f65b6dabecf
ForgeDom <maxstill2006@gmail.com>
2025-02-25 20:10:13 +0200

    +

--- Classes/Class.cs
+++ Classes/Class.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Inheritanc.Classes
 {
-    internal class Class
+    public class Device
     {
+        public string Name { get; set; }
+        public string Sound { get; set; }
+        public string Show { get; set; }
+        public string Description { get; set; }
+
+        public Device() { }
+
+        public Device(string name, string sound, string show, string description)
+        {
+            Name = name;
+            Sound = sound;
+            Show = show;
+            Description = description;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine(Sound);
+        }
+
+        public void DisplayShow()
+        {
+            Console.WriteLine(Show);
+        }
+
+        public void DisplayDescription()
+        {
+            Console.WriteLine(Description);
+        }
+    }
+
+    public class Kettle : Device
+    {
+        public Kettle(string name, string sound, string show, string description)
+            : base(name, sound, show, description) { }
+    }
+
+    public class Microwave : Device
+    {
+        public Microwave(string name, string sound, string show, string description)
+            : base(name, sound, show, description) { }
+    }
+
+    public class Car : Device
+    {
+        public Car(string name, string sound, string show, string description)
+            : base(name, sound, show, description) { }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Inheritanc
     {
         static void Main(string[] args)
         {
-            
+            Kettle kettle = new Kettle("Bravo", "Ketlle is making sound", "Kettle", "An electric kettle");
+            Microwave microwave = new Microwave("Samsung", "Microwave is making sound", "Microwave", "A microwave oven");
+            Car car = new Car("Toyota", "Car is making sound", "Car", "A car");
+
+
+            DisplayDeviceDetails(kettle);
+            DisplayDeviceDetails(microwave);
+            DisplayDeviceDetails(car);
+        }
+        static void DisplayDeviceDetails(Device device)
+        {
+            device.DisplayShow();
+            device.MakeSound();
+            device.DisplayDescription();
+            Console.WriteLine();
         }
     }
+    
 }
